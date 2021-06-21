2033115ca24bdc0ae31770e9cdd669f89f9dd364
sazonovme <mr.loraks@bk.ru>
2021-06-21 21:50:52 +0600

    create: b-reviews / slider (html,style,js)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider  ', {
   loop: true,
   // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
+  },
+});
+
+const reviewsSlider = new Swiper('.reviews-slider  ', {
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 });
